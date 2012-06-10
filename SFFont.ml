a3fd6331c7f70df658a3dca47b62ea0eb829a75b
blue_prawn <blue_prawn@bdb394e1-c25e-40ac-8a3d-3399da013453>
2012-06-10 16:59:14 +0000

    we should not call sfFont_Destroy() on the DefaultFont
    git-svn-id: svn://scm.ocamlcore.org/svn/ocaml-sfml/trunk@22 bdb394e1-c25e-40ac-8a3d-3399da013453

--- SFFont.ml
+++ SFFont.ml
@@ -1,5 +1,5 @@
 type u
-type t = { u:u; s:string }
+type t = { u:u; s:string; is_default:bool }
 
 external getDefaultFont: unit -> u = "caml_sfFont_GetDefaultFont"
 
@@ -13,22 +13,26 @@ external createFromMemory:
 
 external destroy: font:u -> unit = "caml_sfFont_Destroy"
 
-let destroy font = destroy font.u
+(* ================ *)
+
+let destroy font =
+  if not (font.is_default)
+  then destroy font.u
 
 let getDefaultFont () =
   let u = getDefaultFont () in
-  let t = {u=u; s=" "} in
+  let t = {u=u; s=" "; is_default=true} in
   Gc.finalise destroy t;
   (t)
 
 let createFromFile ?charSize ?charset filename =
   let u = createFromFile ?charSize ?charset filename in
-  let t = {u=u; s=" "} in
+  let t = {u=u; s=" "; is_default=false} in
   Gc.finalise destroy t;
   (t)
 
 let createFromMemory ?charSize ?charset buffer =
   let u = createFromMemory ?charSize ?charset buffer in
-  let t = {u=u; s=" "} in
+  let t = {u=u; s=" "; is_default=false} in
   Gc.finalise destroy t;
   (t)
