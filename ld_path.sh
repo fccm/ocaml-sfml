5a8922a5e137d2360f10d7b72940cefc26d9609a
Blue Prawn <monnier.florent@gmail.com>
2013-08-04 20:12:57 +0200

    clean from devel files

--- ld_path.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/blue_prawn/temp/SFML_git/lib:/temp/CSFML_git/lib"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/blue_prawn/temp/SFML_git/lib"
--- sfeMovie/ld_path.sh
+++
@@ -1,1 +0,0 @@
-export LD_LIBRARY_PATH=/temp/LaurentGomila-SFML-757a785/lib:/temp/sfeMovie/lib
