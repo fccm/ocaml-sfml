0ae7b8c043c123d58d87a62a3c295fb2648a1499
Blue Prawn <monnier.florent@gmail.com>
2013-08-06 23:15:35 +0200

    convert mouse buttons variant to/from strings

--- src/SFMouse.ml
+++ src/SFMouse.ml
@@ -12,3 +12,20 @@ external isButtonPressed: button:mouse_button -> bool
 external getPosition: unit -> int * int = "caml_sfMouse_getPosition"
 external setPosition: pos:int * int -> unit = "caml_sfMouse_setPosition"
 (** for the [relativeTo] parameters, see the module [SFWindow] *)
+
+let string_of_button = function
+  | ButtonLeft    -> "ButtonLeft"
+  | ButtonRight   -> "ButtonRight"
+  | ButtonMiddle  -> "ButtonMiddle"
+  | ButtonX1      -> "ButtonX1"
+  | ButtonX2      -> "ButtonX2"
+
+let button_of_string s =
+  match String.lowercase s with
+  | "buttonleft"    -> ButtonLeft
+  | "buttonright"   -> ButtonRight
+  | "buttonmiddle"  -> ButtonMiddle
+  | "buttonx1"      -> ButtonX1
+  | "buttonx2"      -> ButtonX2
+  | _ -> invalid_arg "SFMouse.button_of_string"
+
