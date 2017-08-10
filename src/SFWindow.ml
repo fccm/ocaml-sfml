88091481ac8ff2f3a573a6fa495576fc69292243
Sylvain Boilard <boilard@crans.org>
2017-08-10 12:41:13 +0200

    Style and reordering in SFWindow.

--- src/SFWindow.ml
+++ src/SFWindow.ml
@@ -4,15 +4,6 @@ external create: mode:SFVideoMode.t -> title:string ->
   style:SFStyle.t list -> settings:SFContextSettings.t -> t
   = "caml_sfWindow_create"
 
-type window_handle = nativeint
-
-external createFromHandle: handle:window_handle -> settings:SFContextSettings.t -> t
-  = "caml_sfWindow_createFromHandle"
-
-external getSystemHandle: t -> window_handle
-  = "caml_sfWindow_getSystemHandle"
-
-
 let make ?(style = SFStyle.default) ?(bpp = 32)
     ?(settings = SFContextSettings.default) (width, height) title =
   let mode =
@@ -24,44 +15,50 @@ let make ?(style = SFStyle.default) ?(bpp = 32)
   in
   (create ~mode ~title ~style ~settings)
 
+type window_handle = nativeint
+
+external createFromHandle:
+  handle:window_handle -> settings:SFContextSettings.t -> t
+  = "caml_sfWindow_createFromHandle"
+
+external close: t -> unit = "caml_sfWindow_close"
 
 external isOpen: t -> bool = "caml_sfWindow_isOpen"
-external close: t -> unit = "caml_sfWindow_close"
-external display: t -> unit = "caml_sfWindow_display"
-
-external pollEvent: t -> SFEvent.t option = "caml_sfWindow_pollEvent"
-external waitEvent: t -> SFEvent.t = "caml_sfWindow_waitEvent"
-
-external getSize: t -> int * int = "caml_sfWindow_getSize"
-external getWidth: t -> int = "caml_sfWindow_getWidth"
-external getHeight: t -> int = "caml_sfWindow_getHeight"
 
 external getSettings: t -> SFContextSettings.t = "caml_sfWindow_getSettings"
 
-external setSize: t -> size:int * int -> unit
-  = "caml_sfWindow_setSize"
+external pollEvent: t -> SFEvent.t option = "caml_sfWindow_pollEvent"
 
-external setSize2: t -> width:int -> height:int -> unit
-  = "caml_sfWindow_setSize2"
+external waitEvent: t -> SFEvent.t = "caml_sfWindow_waitEvent"
 
-external setPosition: t -> pos:int * int -> unit
-  = "caml_sfWindow_setPosition"
+external getPosition: t -> int * int = "caml_sfWindow_getPosition"
+
+external setPosition: t -> pos:int * int -> unit = "caml_sfWindow_setPosition"
 
 external setPosition2: t -> left:int -> top:int -> unit
   = "caml_sfWindow_setPosition2"
 
-external getPosition: t -> int * int = "caml_sfWindow_getPosition"
+external getSize: t -> int * int = "caml_sfWindow_getSize"
+
+external getWidth: t -> int = "caml_sfWindow_getWidth"
+
+external getHeight: t -> int = "caml_sfWindow_getHeight"
+
+external setSize: t -> size:int * int -> unit = "caml_sfWindow_setSize"
+
+external setSize2: t -> width:int -> height:int -> unit
+  = "caml_sfWindow_setSize2"
 
 external setTitle: t -> title:string -> unit = "caml_sfWindow_setTitle"
-
-external setActive: t -> active:bool -> bool = "caml_sfWindow_setActive"
-
+(*
+external setIcon: t -> size:int * int -> pixels:bytes -> unit
+  = "caml_sfWindow_setIcon"
+*)
 external setVisible: t -> visible:bool -> unit = "caml_sfWindow_setVisible"
 
-external requestFocus: t -> unit = "caml_sfWindow_requestFocus"
-                                               
-external hasFocus: t -> bool = "caml_sfWindow_hasFocus"
-                                               
+external setVerticalSyncEnabled: t -> enabled:bool -> unit
+  = "caml_sfWindow_setVerticalSyncEnabled"
+
 external setMouseCursorVisible: t -> visible:bool -> unit
   = "caml_sfWindow_setMouseCursorVisible"
 
@@ -71,15 +68,25 @@ external setMouseCursorGrabbed: t -> grabbed:bool -> unit
 external setKeyRepeatEnabled: t -> enabled:bool -> unit
   = "caml_sfWindow_setKeyRepeatEnabled"
 
-external setJoystickThreshold: t -> threshold:float -> unit
-  = "caml_sfWindow_setJoystickThreshold"
-
 external setFramerateLimit: t -> limit:int -> unit
   = "caml_sfWindow_setFramerateLimit"
 
-external setVerticalSyncEnabled: t -> enabled:bool -> unit
-  = "caml_sfWindow_setVerticalSyncEnabled"
+external setJoystickThreshold: t -> threshold:float -> unit
+  = "caml_sfWindow_setJoystickThreshold"
 
-external getMousePosition: relativeTo:t -> int * int = "caml_sfWindow_getMousePosition"
-external setMousePosition: relativeTo:t -> int * int -> unit = "caml_sfWindow_setMousePosition"
-(** these functions are also in [SFMouse] but without the [relativeTo] parameter *)
+external setActive: t -> active:bool -> bool = "caml_sfWindow_setActive"
+
+external requestFocus: t -> unit = "caml_sfWindow_requestFocus"
+
+external hasFocus: t -> bool = "caml_sfWindow_hasFocus"
+
+external display: t -> unit = "caml_sfWindow_display"
+
+external getSystemHandle: t -> window_handle = "caml_sfWindow_getSystemHandle"
+
+(** These functions are also in [SFMouse] but without the [relativeTo] parameter *)
+external getMousePosition: relativeTo:t -> int * int
+  = "caml_sfWindow_getMousePosition"
+
+external setMousePosition: relativeTo:t -> int * int -> unit
+  = "caml_sfWindow_setMousePosition"
--- src/cxx_stubs/SFWindow_stub.cpp
+++ src/cxx_stubs/SFWindow_stub.cpp
@@ -101,13 +101,6 @@ caml_sfWindow_createFromHandle(value ml_handle, value ml_settings)
 }
 
 CAMLextern_C value
-caml_sfWindow_isOpen(value win)
-{
-    return Val_bool(
-        SfWindow_val(win)->isOpen());
-}
-
-CAMLextern_C value
 caml_sfWindow_close(value win)
 {
     SfWindow_val(win)->close();
@@ -115,12 +108,17 @@ caml_sfWindow_close(value win)
 }
 
 CAMLextern_C value
-caml_sfWindow_display(value win)
+caml_sfWindow_isOpen(value win)
 {
-    caml_enter_blocking_section();
-    SfWindow_val(win)->display();
-    caml_leave_blocking_section();
-    return Val_unit;
+    return Val_bool(
+        SfWindow_val(win)->isOpen());
+}
+
+CAMLextern_C value
+caml_sfWindow_getSettings(value win)
+{
+    sf::ContextSettings settings = SfWindow_val(win)->getSettings();
+    return Val_sfContextSettings(&settings);
 }
 
 CAMLextern_C value
@@ -141,6 +139,27 @@ caml_sfWindow_waitEvent(value win)
     bool err = SfWindow_val(win)->waitEvent(event);
     if (err) caml_failwith(func_name);
     return Val_sfEvent(event, func_name);
+}
+
+CAMLextern_C value
+caml_sfWindow_getPosition(value win)
+{
+    sf::Vector2i pos = SfWindow_val(win)->getPosition();
+    return Val_sfVector2i(pos);
+}
+
+CAMLextern_C value
+caml_sfWindow_setPosition(value win, value pos)
+{
+    SfWindow_val(win)->setPosition(SfVector2i_val(pos));
+    return Val_unit;
+}
+
+CAMLextern_C value
+caml_sfWindow_setPosition2(value win, value left, value top)
+{
+    SfWindow_val(win)->setPosition(SfVector2i_val2(left, top));
+    return Val_unit;
 }
 
 CAMLextern_C value
@@ -165,13 +184,6 @@ caml_sfWindow_getHeight(value win)
 }
 
 CAMLextern_C value
-caml_sfWindow_setSize2(value win, value width, value height)
-{
-    SfWindow_val(win)->setSize(SfVector2u_val2(width, height));
-    return Val_unit;
-}
-
-CAMLextern_C value
 caml_sfWindow_setSize(value win, value size)
 {
     SfWindow_val(win)->setSize(SfVector2u_val(size));
@@ -179,24 +191,10 @@ caml_sfWindow_setSize(value win, value size)
 }
 
 CAMLextern_C value
-caml_sfWindow_setPosition2(value win, value left, value top)
+caml_sfWindow_setSize2(value win, value width, value height)
 {
-    SfWindow_val(win)->setPosition(SfVector2i_val2(left, top));
+    SfWindow_val(win)->setSize(SfVector2u_val2(width, height));
     return Val_unit;
-}
-
-CAMLextern_C value
-caml_sfWindow_setPosition(value win, value pos)
-{
-    SfWindow_val(win)->setPosition(SfVector2i_val(pos));
-    return Val_unit;
-}
-
-CAMLextern_C value
-caml_sfWindow_getPosition(value win)
-{
-    sf::Vector2i pos = SfWindow_val(win)->getPosition();
-    return Val_sfVector2i(pos);
 }
 
 CAMLextern_C value
@@ -205,14 +203,10 @@ caml_sfWindow_setTitle(value win, value title)
     SfWindow_val(win)->setTitle(String_val(title));
     return Val_unit;
 }
-
-CAMLextern_C value
-caml_sfWindow_setActive(value win, value active)
-{
-    bool a = SfWindow_val(win)->setActive(Bool_val(active));
-    return Val_bool(a);
-}
-
+/* TODO
+void sfWindow_setIcon(sfWindow* window,
+        unsigned int width, unsigned int height, const sfUint8* pixels);
+*/
 CAMLextern_C value
 caml_sfWindow_setVisible(value win, value visible)
 {
@@ -221,16 +215,10 @@ caml_sfWindow_setVisible(value win, value visible)
 }
 
 CAMLextern_C value
-caml_sfWindow_requestFocus(value win)
+caml_sfWindow_setVerticalSyncEnabled(value win, value enabled)
 {
-    SfWindow_val(win)->requestFocus();
+    SfWindow_val(win)->setVerticalSyncEnabled(Bool_val(enabled));
     return Val_unit;
-}
-
-CAMLextern_C value
-caml_sfWindow_hasFocus(value win)
-{
-    return Val_bool(SfWindow_val(win)->hasFocus());
 }
 
 CAMLextern_C value
@@ -255,13 +243,6 @@ caml_sfWindow_setKeyRepeatEnabled(value win, value enabled)
 }
 
 CAMLextern_C value
-caml_sfWindow_setJoystickThreshold(value win, value threshold)
-{
-    SfWindow_val(win)->setJoystickThreshold(Double_val(threshold));
-    return Val_unit;
-}
-
-CAMLextern_C value
 caml_sfWindow_setFramerateLimit(value win, value limit)
 {
     SfWindow_val(win)->setFramerateLimit(Long_val(limit));
@@ -269,17 +250,46 @@ caml_sfWindow_setFramerateLimit(value win, value limit)
 }
 
 CAMLextern_C value
-caml_sfWindow_setVerticalSyncEnabled(value win, value enabled)
+caml_sfWindow_setJoystickThreshold(value win, value threshold)
 {
-    SfWindow_val(win)->setVerticalSyncEnabled(Bool_val(enabled));
+    SfWindow_val(win)->setJoystickThreshold(Double_val(threshold));
     return Val_unit;
 }
 
 CAMLextern_C value
-caml_sfWindow_getSettings(value win)
+caml_sfWindow_setActive(value win, value active)
 {
-    sf::ContextSettings settings = SfWindow_val(win)->getSettings();
-    return Val_sfContextSettings(&settings);
+    bool a = SfWindow_val(win)->setActive(Bool_val(active));
+    return Val_bool(a);
+}
+
+CAMLextern_C value
+caml_sfWindow_requestFocus(value win)
+{
+    SfWindow_val(win)->requestFocus();
+    return Val_unit;
+}
+
+CAMLextern_C value
+caml_sfWindow_hasFocus(value win)
+{
+    return Val_bool(SfWindow_val(win)->hasFocus());
+}
+
+CAMLextern_C value
+caml_sfWindow_display(value win)
+{
+    caml_enter_blocking_section();
+    SfWindow_val(win)->display();
+    caml_leave_blocking_section();
+    return Val_unit;
+}
+
+CAMLextern_C value
+caml_sfWindow_getSystemHandle(value win)
+{
+    sf::WindowHandle handle = SfWindow_val(win)->getSystemHandle();
+    return caml_copy_nativeint(handle);
 }
 
 CAMLextern_C value
@@ -295,19 +305,5 @@ caml_sfWindow_setMousePosition(value relativeTo, value position)
     sf::Mouse::setPosition(SfVector2i_val(position), *SfWindow_val(relativeTo));
     return Val_unit;
 }
-
-CAMLextern_C value
-caml_sfWindow_getSystemHandle(value win)
-{
-    sf::WindowHandle handle = SfWindow_val(win)->getSystemHandle();
-    return caml_copy_nativeint(handle);
-}
-
-/* TODO
-void sfWindow_setSize(sfWindow* window, sfVector2u size);
-
-void sfWindow_setIcon(sfWindow* window,
-        unsigned int width, unsigned int height, const sfUint8* pixels);
-*/
 
 // vim: sw=4 sts=4 ts=4 et
