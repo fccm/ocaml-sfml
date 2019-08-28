d195d8de166bb1722dd79de8197c68ee0c1a910c
Florent Monnier <monnier.florent@gmail.com>
2019-08-28 12:59:28 +0159

    updated "ex_rect_oo.sh" boot script

--- examples/ex_rect_oo.sh
+++ examples/ex_rect_oo.sh
@@ -1,8 +1,6 @@
-ocaml bigarray.cma \
+ocaml -I ../src \
   sfml_system.cma \
-  sfml_system_oo.cma \
   sfml_window.cma \
-  sfml_window_oo.cma \
   sfml_graphics.cma \
   sfml_graphics_oo.cma \
   ex_rect_oo.ml
