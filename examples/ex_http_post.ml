0dfaac9b973df0ca961cdcbea79bcc222b0a95f1
Florent Monnier <monnier.florent@gmail.com>
2019-08-29 03:50:55 +0159

    updated example "ex_http_post.ml"

--- examples/ex_http_post.ml
+++ examples/ex_http_post.ml
@@ -5,7 +5,7 @@ let () =
   SFHttp.Request.setMethod request SFHttp.Post;
   SFHttp.Request.setUri request "/w/index.php?title=Special:Export&action=submit";
   SFHttp.Request.setBody request "OCaml";
-  SFHttp.setHost http "en.wikipedia.org" 0;
+  SFHttp.setHost http ~host:"en.wikipedia.org" ~port:0 ();
   let timeout = SFTime.of_seconds 5.0 in
   let r = SFHttp.sendRequest http ~request ~timeout () in
   print_endline (SFHttp.Response.getBody r)
