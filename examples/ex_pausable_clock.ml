90d3b637bd91f3acf2fe419256455cd01e2fd674
Florent Monnier <monnier.florent@gmail.com>
2019-08-29 03:03:58 +0159

    updated example ex_pausable_clock.ml

--- examples/ex_pausable_clock.ml
+++ examples/ex_pausable_clock.ml
@@ -12,14 +12,13 @@ let main () =
   print_is_paused pc;
   SFTime.sleep (SFTime.of_seconds 0.2);
   print_elapsed_time pc;
-  let _ = SFPausableClock.restart ~clock:pc ~paused:false in
+  let _ = SFPausableClock.restart pc ~paused:false in
   SFTime.sleep (SFTime.of_seconds 0.2);
   SFPausableClock.pause pc;
   SFTime.sleep (SFTime.of_seconds 0.8);
   SFPausableClock.start pc;
   SFTime.sleep (SFTime.of_seconds 0.4);
   print_elapsed_time pc;
-  SFPausableClock.destroy pc;
 ;;
 
 let () =
