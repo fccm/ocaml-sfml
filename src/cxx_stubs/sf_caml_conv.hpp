4a33f7b499515b13179e3c32ba62967a2dceef96
Florent Monnier <monnier.florent@gmail.com>
2022-07-25 22:05:28 +0200

    guards for macros that appeared upstream

--- src/cxx_stubs/sf_caml_conv.hpp
+++ src/cxx_stubs/sf_caml_conv.hpp
@@ -2,10 +2,19 @@
 #define _SF_CAML_CONV_INC
 
 
+#if OCAML_VERSION < 41200
+
 #define Val_none Val_int(0)
 #define Some_val(v) Field(v,0)
 
 value Val_some(value v);
+
+#else
+
+#define Val_some caml_alloc_some
+
+#endif
+
 
 #define Option_val(ml_value, Conv_val, default_value) \
     (ml_value != Val_none ? Conv_val(Some_val(ml_value)) : default_value)
